d178a6d3843ead501eabb539f8655bd7dc564572
awansetyawan <alifm6331@gmail.com>
2023-03-06 11:09:29 +0800

    Posttest 1

---
+++ Posttest1/src/Laptop.java
@@ -0,0 +1,19 @@
+public class Laptop {
+    String merek;
+    String seri;
+    String namapemilik;
+    String nomorhp;
+    String kerusakan;
+    int hargaperbaikan;
+    int teknisi;
+
+    public Laptop(String merek, String seri, String namapemilik, String nomorhp, String kerusakan, int hargaperbaikan, int teknisi){
+        this.merek = merek;
+        this.seri = seri;
+        this.namapemilik = namapemilik;
+        this.nomorhp = nomorhp;
+        this.kerusakan = kerusakan;
+        this.hargaperbaikan = hargaperbaikan;
+        this.teknisi = teknisi;
+    }
+}
---
+++ Posttest1/src/Teknisi.java
@@ -0,0 +1,11 @@
+public class Teknisi {
+    String nama;
+    String gender;
+    String nomorhp;
+
+    public Teknisi (String nama, String gender, String nomorhp){
+        this.nama = nama;
+        this.gender = gender;
+        this.nomorhp = nomorhp;
+    }
+}
